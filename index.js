de83749bd32744069365639e4dd3eb205f466f1b
daily-dev-jake <jlele92@outlook.com>
2023-02-02 23:58:11 +0800

    Added UI elements

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const ages = { alice: 18, bob: 27 };
+console.log(ages);
+const button = document.querySelector('input');
+const paragraph = document.querySelector('p');
+
+button.addEventListener('click', updateButton);
+
+function updateButton() {
+  if (button.value === 'Start machine') {
+    button.value = 'Stop machine';
+    paragraph.textContent = 'The machine has started!';
+  } else {
+    button.value = 'Start machine';
+    paragraph.textContent = 'The machine is stopped.';
+  }
+}
